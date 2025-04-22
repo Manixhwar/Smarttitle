de89ce3af025a56e08d425189f53730f34bac2b6
Manixhwar <shrimanish2797@gmail.com>
2025-04-23 01:04:55 +0530

    missed update
    missed update

--- src/main/java/com/smarttitle/smarttitle_backend/SmarttitleBackendApplication.java
+++ src/main/java/com/smarttitle/smarttitle_backend/SmarttitleBackendApplication.java
@@ -9,6 +9,8 @@ public class SmarttitleBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmarttitleBackendApplication.class, args);
 		System.out.println("Main Services Starts");
+		System.out.println(" second update");
+
 	}
 
 }
